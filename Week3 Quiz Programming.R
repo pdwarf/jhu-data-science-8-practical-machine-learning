8c7ff57ead78b201b1a751082c468cecf5457f47
David Arens <davidarens@me.com>
2018-10-23 08:23:04 +0200

    worked on quiz 3

--- Week3 Quiz Programming.R
+++ Week3 Quiz Programming.R
@@ -13,3 +13,17 @@ modelFit <- train(Class ~ ., method = "rpart", data = training)
 fancyRpartPlot(modelFit$finalModel)
 
 #newCases <- PS, WS, PS, not possible
+
+
+#Q3
+library(pgmm)
+library(caret)
+data(olive)
+olive = olive[,-1]
+
+modelFit <- train(Area ~ ., method = "rpart", data = olive)
+
+newdata = as.data.frame(t(colMeans(olive)))
+
+predict(modelFit, newdata)
+#Strange, b/c area should be qualitative, not numeric
