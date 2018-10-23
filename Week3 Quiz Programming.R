b8dc2705852c179ceed26718978560309e02cddb
David Arens <davidarens@me.com>
2018-10-23 08:11:49 +0200

    cleaned quiz 2

--- Week3 Quiz Programming.R
+++ Week3 Quiz Programming.R
@@ -4,10 +4,6 @@ data(segmentationOriginal)
 library(caret)
 library(rattle)
 
-#inTrain <- createDataPartition(y = segmentationOriginal$Class, p = 0.7, list = F)
-#training <- segmentationOriginal[Case = "Test",]
-#testing <- segmentationOriginal[-inTrain,]
-
 training <- subset(segmentationOriginal, Case == "Train")
 
 set.seed(125)
