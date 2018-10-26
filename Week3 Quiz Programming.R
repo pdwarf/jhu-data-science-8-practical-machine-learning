006b431e35d90aa2f881e3661fe55a02782d2015
David Arens <davidarens@me.com>
2018-10-26 08:30:54 +0200

    worked on quiz 3- q4

--- Week3 Quiz Programming.R
+++ Week3 Quiz Programming.R
@@ -30,6 +30,8 @@ predict(modelFit, newdata)
 
 #Q4
 library(ElemStatLearn)
+library(caret)
+
 data(SAheart)
 set.seed(8484)
 train = sample(1:dim(SAheart)[1],size=dim(SAheart)[1]/2,replace=F)
@@ -43,3 +45,28 @@ missClass = function(values,prediction){sum(((prediction > 0.5)*1) != values)/le
 
 missClass(trainSA$chd, predict(modelFit, trainSA)) #[1] 0.2727273
 missClass(testSA$chd, predict(modelFit, testSA)) #[1] 0.3116883
+
+#Q5
+
+library(ElemStatLearn)
+#library(caret)
+library(randomForest)
+
+data(vowel.train)
+data(vowel.test)
+
+vowel.test$y <- as.factor(vowel.test$y)
+vowel.train$y <- as.factor(vowel.train$y)
+
+vowel.complete <- rbind(vowel.train, vowel.test)
+
+set.seed(33833)
+
+modelFit <- randomForest(y ~ ., data = vowel.train)
+varImp(modelFit)
+order(varImp(modelFit), decreasing = T)
+
+#updates in the packages seem to have changed the values; 
+#my results are most similar to the option in the quiz 
+#"The order of the variables is: 
+#x.2, x.1, x.5, x.6, x.8, x.4, x.9, x.3, x.7,x.10"
