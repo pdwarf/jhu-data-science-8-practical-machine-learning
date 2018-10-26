9a11ec7674f2e22cf3267924f95166abe3621867
David Arens <davidarens@me.com>
2018-10-26 07:39:56 +0200

    worked on quiz 3- q4

--- Week3 Quiz Programming.R
+++ Week3 Quiz Programming.R
@@ -27,3 +27,19 @@ newdata = as.data.frame(t(colMeans(olive)))
 
 predict(modelFit, newdata)
 #Strange, b/c area should be qualitative, not numeric
+
+#Q4
+library(ElemStatLearn)
+data(SAheart)
+set.seed(8484)
+train = sample(1:dim(SAheart)[1],size=dim(SAheart)[1]/2,replace=F)
+trainSA = SAheart[train,]
+testSA = SAheart[-train,]
+
+set.seed(13234)
+modelFit <- train(chd ~ age + alcohol + obesity + tobacco + typea + ldl, method = "glm", family = "binomial", data = trainSA)
+
+missClass = function(values,prediction){sum(((prediction > 0.5)*1) != values)/length(values)}
+
+missClass(trainSA$chd, predict(modelFit, trainSA)) #[1] 0.2727273
+missClass(testSA$chd, predict(modelFit, testSA)) #[1] 0.3116883
